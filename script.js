dc9d6ae9dfc808fd83620923cd26157adb196073
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-05-30 22:18:33 +0300

    3

---
+++ script.js
@@ -0,0 +1,26 @@
+
+$(".second__about__toggle__title__2").on("click", function(){
+    $(".second__about__toggle__articles__2").fadeIn(1000);
+    $(".second__about__toggle__articles__1").fadeOut(500);
+    $(".second__about__toggle__articles__3").fadeOut(500);
+    $(".second__about__toggle__title__3").removeClass("active");
+    $(".second__about__toggle__title__1").removeClass("active");
+    $(this).addClass("active")
+})
+$(".second__about__toggle__title__1").on("click", function(){
+    $(".second__about__toggle__articles__1").fadeIn(1000);
+    $(".second__about__toggle__articles__2").fadeOut(500);
+    $(".second__about__toggle__articles__3").fadeOut(500);
+    $(".second__about__toggle__title__3").removeClass("active");
+    $(".second__about__toggle__title__2").removeClass("active");
+    $(this).addClass("active")
+});
+
+$(".second__about__toggle__title__3").on("click", function(){
+    $(".second__about__toggle__articles__3").fadeIn(1000);
+    $(".second__about__toggle__articles__2").fadeOut(500);
+    $(".second__about__toggle__articles__1").fadeOut(500);
+    $(".second__about__toggle__title__2").removeClass("active");
+    $(".second__about__toggle__title__1").removeClass("active");
+    $(this).addClass("active");
+})
