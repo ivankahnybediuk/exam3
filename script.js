00bee184b47a88003f02a0372e249fb1e1717759
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-05-31 14:05:15 +0300

    6

--- script.js
+++ script.js
@@ -39,16 +39,29 @@ $('.second__products__slider').slick({
   });
 
 //   ====================Akordeon==================================
-
-$(document).ready(function() {
     $('.second__faqs__akordeon__title').on('click', function(){
       $('.second__faqs__akordeon__article').not($(this).next()).slideUp(500);
       $(this).next().slideToggle(500)
     })
-})
  
+    // =====================Slider_review==================
+    $('.reviews__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1, 
+        prevArrow: (".rewiews__arrows__prev"),
+        nextArrow: (".rewiews__arrows__next"),
+        autoplay: true
+      });
 
 
+    //   ==================Scroll=============================
+    $(".header").on("click","a", function (event) {
+        event.preventDefault();
+        let id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 2000);
+    });
 
 
 })
